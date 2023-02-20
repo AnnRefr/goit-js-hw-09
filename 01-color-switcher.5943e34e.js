fe5646c2b5c1196af848aa91b46599e8a23a0f39
AnnRefr <108008551+AnnRefr@users.noreply.github.com>
2023-02-20 14:31:05 +0000

    Deploying to gh-pages from @ AnnRefr/goit-js-hw-09@8d477e6ca54c3eb8a8409698acbbbf4a895f51a4 🚀

--- 01-color-switcher.5943e34e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("body"),t=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),r.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.5943e34e.js.map
